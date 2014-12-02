df0bce6b227dad8c8538a8ac69831ab53774996a
FelixMcFelix <kyleandrew.simpson@gmail.com>
2014-12-02 15:36:03 +0000

    I'm... not entirely sure what I changed. Repo committed before I get home anyhow.

--- control-pane.js
+++ control-pane.js
@@ -6,19 +6,17 @@ window.controlPane = new (function(){
 		sel1.options.length = 0;
 		sel2.options.length = 0;
 
-		console.log(Model.data.referendum[0].length+", "+Model.data.simd[0].length);
-
 		for(i = 0; i<Model.data.referendum[0].length+Model.data.simd[0].length; i++){
 			var str;
 			
 			if(i<Model.data.referendum[0].length){
 				str = Model.data.referendum[0][i];
-				sel1.options[i] = new Option("Referendum - " + str, str, false, false);
-				sel2.options[i] = new Option("Referendum - " + str, str, false, false);
+				sel1.options[i] = new Option("Referendum - " + str, "ref"+i, false, false);
+				sel2.options[i] = new Option("Referendum - " + str, "ref"+i, false, false);
 			}	else{
 				str = Model.data.simd[0][i-Model.data.referendum[0].length];
-				sel1.options[i] = new Option("SIMD - " + str, str, false, false);
-				sel2.options[i] = new Option("SIMD - " + str, str, false, false);
+				sel1.options[i] = new Option("SIMD - " + str, "simd"+i, false, false);
+				sel2.options[i] = new Option("SIMD - " + str, "simd"+i, false, false);
 			}
 			
 		}
