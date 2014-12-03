b1fa916e112e4925702eea93d7326c24ec642765
FelixMcFelix <kyleandrew.simpson@gmail.com>
2014-12-03 02:05:21 +0000

    Removal of county name columns, addition of Custom columns
    Custom columns will work by appending data to rows, and can be accessed
    by the specification ref/simd<row> as a link between column and name.

--- control-pane.js
+++ control-pane.js
@@ -6,19 +6,31 @@ window.controlPane = new (function(){
 		sel1.options.length = 0;
 		sel2.options.length = 0;
 
-		for(i = 0; i<Model.data.referendum[0].length+Model.data.simd[0].length; i++){
+		for(i = 0, intInd = 0; i<Model.data.referendum[0].length+Model.data.simd[0].length; i++){
 			var str;
+			var simdStart = Model.data.referendum[0].length;
+			var simdInd = i-simdStart;
 			
-			if(i<Model.data.referendum[0].length){
+			if(i<simdStart && i!=0){
 				str = Model.data.referendum[0][i];
-				sel1.options[i] = new Option("Referendum - " + str, "ref"+i, false, false);
-				sel2.options[i] = new Option("Referendum - " + str, "ref"+i, false, false);
-			}	else{
-				str = Model.data.simd[0][i-Model.data.referendum[0].length];
-				sel1.options[i] = new Option("SIMD - " + str, "simd"+i, false, false);
-				sel2.options[i] = new Option("SIMD - " + str, "simd"+i, false, false);
+				sel1.options[intInd] = new Option("Referendum - " + str, "ref"+i, false, false);
+				sel2.options[intInd] = new Option("Referendum - " + str, "ref"+i, false, false);
+				intInd++;
+			}	else if(i>simdStart){
+				str = Model.data.simd[0][simdInd];
+				sel1.options[intInd] = new Option("SIMD - " + str, "simd"+simdInd, false, false);
+				sel2.options[intInd] = new Option("SIMD - " + str, "simd"+simdInd, false, false);
+				intInd++;
 			}
-			
 		}
+		//Add CUSTOM boxes to each selector. These
+		sel1.options[sel1.options.length] = new Option("Custom 1", "simd"+Model.data.simd[0].length, false, false);
+		sel2.options[sel2.options.length] = new Option("Custom 1", "simd"+Model.data.simd[0].length, false, false);
+		sel1.options[sel1.options.length] = new Option("Custom 2", "simd"+(Model.data.simd[0].length+1), false, false);
+		sel2.options[sel2.options.length] = new Option("Custom 2", "simd"+(Model.data.simd[0].length+1), false, false);
+
+		//Match initial model state to initial selectorbox state.
+		Model.comparison.var1 = "ref1";
+		Model.comparison.var2 = "ref1";
 	}
 })();
