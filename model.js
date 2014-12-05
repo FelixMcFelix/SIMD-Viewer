def773a09d85eaa2cbf08f12ddadc70eef2ad978
FelixMcFelix <kyleandrew.simpson@gmail.com>
2014-12-05 01:56:03 +0000

    Fixing old bug.
    lets just say you shouldn't use parseInt on floats. GOD I'm retarded.
    also I fixed the representation to always be two decimal places, so
    that's nice.

--- model.js
+++ model.js
@@ -87,15 +87,15 @@ window.Model = new (function(){
 			//Change x and y as necessary.
 			//Detect what we are even doing.
 			if(Model.comparison.var1.charAt(0) == "r"){
-				constObj.x = toNumber(constObj.allRef[col1]);
+				constObj.x = (toNumber(constObj.allRef[col1])).toFixed(2);
 			} else{
-				constObj.x = toNumber(constObj.allSIMD[col1]);
+				constObj.x = (toNumber(constObj.allSIMD[col1])).toFixed(2);
 			}
 
 			if(Model.comparison.var2.charAt(0) == "r"){
-				constObj.y = toNumber(constObj.allRef[col2]);
+				constObj.y = (toNumber(constObj.allRef[col2])).toFixed(2);
 			} else{
-				constObj.y = toNumber(constObj.allSIMD[col2]);
+				constObj.y = (toNumber(constObj.allSIMD[col2])).toFixed(2);
 			}
 		
 		}
@@ -185,5 +185,5 @@ window.Constituency = function(name, refRow, simdRow){
 window.toNumber = function(num){
 	num = num.replace(",","");
 	num = num.replace(" ","");
-	return parseInt(num, 10);
+	return parseFloat(num, 10);
 }
